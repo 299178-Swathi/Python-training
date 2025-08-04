54e28609fdd3299cf48b21b8ac7301bfa1f37bd5
299178-Swathi <Swathi.M@ust.com>
2025-08-04 15:07:23 +0530

    Uploaded exercise1.py (test upload)

---
+++ exercise1.py
@@ -0,0 +1,23 @@
+# sum=0
+# while True:
+#     num=(input('enter number:' )
+#     li=['0','1','2','3','4','5','6','7','8','9']
+#     for c in num:
+#          if c not in li:
+#          num =
+#
+
+li=[]
+while True:
+    num=input('enter the number:')
+    if num=='0':
+        break
+    li.append(num)
+
+sum=0
+for number in li:
+    tracker=False
+    li=['0','1','2','3','4','5','6','7','8','9']
+    number=number
+print(sum)
+
